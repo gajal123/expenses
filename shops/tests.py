e527ffc65e3c2a216d8b586779cdfbeedd7e66a1
gazal <gajal.agarwala4@gmail.com>
2020-01-18 20:27:47 +0530

    unit test to test store model

--- shops/tests.py
+++ shops/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+from .models import Store
+from .serializers import StoreSerializer
+
 # Create your tests here.
+
+
+class StoreTest(TestCase):
+    """ Test odule for Store Model"""
+
+    def setUp(self):
+        Store.objects.create(
+            name='Bakia Nathan', address='Indiranagar', city='Bangalore'
+        )
+        Store.objects.create(
+            name='Juice shop', address='Bellandur', city='Bangalore'
+        )
+
+    def test_store_city(self):
+        store_bakia_nathan = Store.objects.get(name='Bakia Nathan')
+        store_juice_shop = Store.objects.get(name='Juice shop')
+        self.assertEqual(store_bakia_nathan.city, store_juice_shop.city)
