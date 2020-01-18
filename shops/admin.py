25327d8cc54b712cbc244d3d3b581c3e7cea2eaf
gazal <gajal.agarwala4@gmail.com>
2020-01-18 19:40:58 +0530

    registered purchase and item models in admin

--- shops/admin.py
+++ shops/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from shops.models import User, Store
+from shops.models import User, Store, Item, Purchase
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
@@ -8,4 +8,14 @@ admin.site.register(User, UserAdmin)
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
+    pass
+
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
     pass
