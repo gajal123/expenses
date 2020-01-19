eef2b31f2f66cad5f37dc17cc006b4ed7adeae82
gazal <gajal.agarwala4@gmail.com>
2020-01-20 01:17:46 +0530

    Added heroku to allowed hosts

--- expenses/settings.py
+++ expenses/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'h6ita3c87d-ap^evz#v86**jj_z6peune)o)gt-2dohh)$ua+f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gazal-expenses.herokuapp.com', 'localhost']
 
 
 # Application definition
