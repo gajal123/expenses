6b19ca727f1397b3a875fcb841c70648da3a9904
gazal <gajal.agarwala4@gmail.com>
2020-01-21 20:30:26 +0530

    url redirection for index page

--- shops/urls.py
+++ shops/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('stores/', views.stores, name='stores'),
     path('stores/', views.stores, name='user_login'),
-    path('index/', views.index, name='dashboard')
+    path('', views.index, name='dashboard')
 ]
